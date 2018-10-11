0fb8ec13e480b371f8a781c995df733282635caf
Sagar Sudhakar <sagarsudhakar@live.com>
2018-10-11 15:07:25 -0500

    Eliminated "magic-like" numbers - BalancedPassword

--- src/CodilityChallenges/BalancedPassword.java
+++ src/CodilityChallenges/BalancedPassword.java
@@ -1,32 +1,32 @@
 package CodilityChallenges;
 
-//Date: 10th October 2018
+// Date: 10th October 2018
 
-//Programmer: Sagar Sudhakar
+// Programmer: Sagar Sudhakar
 
-//A string is balanced if it consists of exactly two different characters and both of those characters appear exactly the same number of times. 
+// A string is balanced if it consists of exactly two different characters and both of those characters appear exactly the same number of times.
 
-//For example: "aabbab" is balanced (both 'a' and 'b' occur three times) but "aabba" is not balanced ('a' occurs three times, 'b' occurs two times). 
+// For example: "aabbab" is balanced (both 'a' and 'b' occur three times) but "aabba" is not balanced ('a' occurs three times, 'b' occurs two times).
 
-//String "aabbcc" is also not balanced (it contains three different letters).
+// String "aabbcc" is also not balanced (it contains three different letters).
 
-//A substring of string S is a string that consists of consecutive letters in S. For example: "ompu" is a substring of "computer" but "cmptr" is not.
+// A substring of string S is a string that consists of consecutive letters in S. For example: "ompu" is a substring of "computer" but "cmptr" is not.
 
-//Write a function solution that, given a string S, returns the length of the longest balanced substring of S.
+// Write a function solution that, given a string S, returns the length of the longest balanced substring of S.
 
-//Examples:
+// Examples:
 
-//1. Given S = "cabbacc", the function should return 4 ("abba" is the longest balanced substring).
+// 1. Given S = "cabbacc", the function should return 4 ("abba" is the longest balanced substring).
 
-//2. Given S = "abababa", the function should return 6 ("ababab" and "bababa" are the longest balanced substrings).
+// 2. Given S = "abababa", the function should return 6 ("ababab" and "bababa" are the longest balanced substrings).
 
-//3. Given S = "aaaaaaa", the function should return 0 (S does not contain a balanced substring).
+// 3. Given S = "aaaaaaa", the function should return 0 (S does not contain a balanced substring).
 
-//Write an efficient algorithm for the following assumptions:
+// Write an efficient algorithm for the following assumptions:
 
-//N is an integer within the range [1..100,000]; - Never understood what 'N' is!
+// N is an integer within the range [1..100,000]; - Never understood what 'N' is!
 
-//string S consists only of lowercase letters (a−z).
+// String S consists only of lowercase letters (a−z).
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -64,53 +64,56 @@ public class BalancedPassword {
 
             if(bufferNotFull(characterBuffer)){
 
-                //if this character is not present
+                // If this character is not present
                 if(!characterBuffer.contains(string.charAt(currentIndex))){
-                    //add character to buffer
+                    // Add character to buffer
                     characterBuffer.add(string.charAt(currentIndex));
                 }
             }
-            //if the buffer size is equal to 2 (never will it be more than 2 as we clear the contents when it = 2 and character is not present)
+            // If the buffer size is equal to 2 (never will it be more than 2 as we clear the contents when it = 2 and character is not present)
             else{
 
-                //if this character is not present
+                // If this character is not present
                 if(!characterBuffer.contains(string.charAt(currentIndex))){
 
-                    //clear contents of the list
+                    // Clear contents of the list
                     characterBuffer.clear();
 
-                    //add previous character - to tail by default - becomes new head
+                    // Add previous character - to tail by default - becomes current head
                     characterBuffer.add(string.charAt(previousCharTo(currentIndex)));
 
-                    //add new character to list - to tail by default
+                    // Add new character to list - to tail by default - current tail
                     characterBuffer.add(string.charAt(currentIndex));
 
-                    //Clear contents of count
+                    // Clear contents of count
                     characterCount.clear();
 
-                    //reinitialize count of previous character to 1
+                    // Reinitialize count of previous character to 1
                     characterCount.put(string.charAt(previousCharTo(currentIndex)),1);
                 }
             }
 
-            //increment count of current character
+            // Also the below section is by logic the common steps no matter how the if condition forks -
+            // As we want to increment count of the character in the current index and also check if the new substring under observation is of maxlength
+
+            // Increment count of current character
             characterCount.put(string.charAt(currentIndex),characterCount.getOrDefault(string.charAt(currentIndex),0)+1);
 
-            //check the current state of affairs - use the buffer and count of characters to see if we have a new maxlength substring each iteration
-            //if found, set max length to newly found/formed substring
+            // Check the current state of affairs - use the buffer and count of characters to see if we have a new maxlength substring each iteration
+            // If found, set max length to newly found/formed substring
             maxLength = Math.max(maxLength,lengthIfBalanced(characterBuffer,characterCount));
         }
         return maxLength;
     }
 
     private static int previousCharTo(int currentIndex) {
-        //Might be an overkill - But readable now and eliminating magic numbers
+        // Might be an overkill - But readable now and eliminating magic numbers
         return currentIndex-1;
     }
 
     private static boolean bufferNotFull(LinkedList<Character> characterBuffer) {
-        //if the buffer size is empty OR
-        //if the buffer size is less than 2.
+        // If the buffer size is empty OR
+        // If the buffer size is less than 2.
         return characterBuffer.size()<2;
     }
 
@@ -124,7 +127,7 @@ public class BalancedPassword {
     }
 
     private static int getLengthOfSubString(LinkedList<Character> characterBuffer, HashMap<Character, Integer> characterCount) {
-        //initialize length to the number of occurrences - (2*occurrences of char a(or b))
+        // Initialize length to the number of occurrences - (2*occurrences of char a(or b))
         return characterCount.get(characterBuffer.getFirst())+characterCount.get(characterBuffer.getLast());
     }
 
