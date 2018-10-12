fb019c74fa86894ebee578a4dd4249891057eed8
Sagar Sudhakar <sagarsudhakar@live.com>
2018-10-12 16:05:06 -0500

    Added MinSum - Solved - Submitted

--- src/LeetCode/TwoSum.java
+++ src/LeetCode/TwoSum.java
@@ -21,7 +21,6 @@ package LeetCode;
         // Because nums[0] + nums[1] = 2 + 7 = 9,
         // return [0, 1].
 
-
 import java.util.HashMap;
 
 public class TwoSum {
@@ -33,43 +32,22 @@ public class TwoSum {
         System.out.println(java.util.Arrays.toString(twoSum(nums, target)));
     }
 
+    // Status: Accepted
+    //Runtime: 4 ms - Beats 99.45% of solutions
     private static int[] twoSum(int[] nums, int target) throws IllegalArgumentException {
-
         HashMap<Integer,Integer> complementMap = new HashMap<>();
-
         for(int index=0;index<nums.length;index++){
-
             int complement = (target) - (nums[index]);
-
-            // System.out.println("Iteration: "+index);
-            // System.out.println("Target: "+target);
-            // System.out.println("Number at Index: "+nums[index]);
-            // System.out.println("Complement: "+complement);
-            // System.out.println(target+" - "+nums[index]+" = "+complement);
-            // System.out.println();
-
-            complementMap.put(complement,index);
-        }
-
-        // System.out.println(complementMap.keySet());
-        // System.out.println(complementMap.values());
-
-        // {2, 7, 11, 15};
-        // [7, 2, -2, -6]
-
-        // [ -2, 2, -6, 7]
-        // [ 2, 1, 3, 0]
-
-        for(int index=0;index<nums.length;index++){
             if(complementMap.containsKey(nums[index])){
                 if(index != complementMap.get(nums[index])){
                     //System.out.println("In here");
-                    return new int[] {index,complementMap.get(nums[index])};
+                    return new int[] {complementMap.get(nums[index]),index};
                 }
             }
+            else{
+                complementMap.put(complement,index);
+            }
         }
-
         return nums;
     }
-
 }
