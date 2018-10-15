ff5159bcefc0cdd3d85d7c553d227ec09e83a254
Sagar Sudhakar <sagarsudhakar@live.com>
2018-10-15 16:16:33 -0500

    Added reverse string - Easy wins.

---
+++ src/DataStructures/SinglyLinkedList.java
@@ -0,0 +1,37 @@
+package DataStructures;
+
+public class SinglyLinkedList<V> {
+
+    private V value;
+
+    private SinglyLinkedList next;
+
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        return -1;
+    }
+
+    private boolean isInvalid(int index) {
+        return true;
+    }
+
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+
+    }
+
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+
+    }
+
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+
+    }
+}
---
+++ src/LeetCode/ReverseString.java
@@ -0,0 +1,83 @@
+package LeetCode;
+// Date: 15th October 2018
+
+// Programmer: Sagar Sudhakar
+
+// Difficulty: Easy
+
+// 344. Reverse String
+
+/*
+*
+*
+Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A"
+*
+*
+* */
+
+
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String inputString = "A man, a plan, a canal: Panama";
+        System.out.println(inputString);
+        //System.out.println(reverseString(inputString));
+        //System.out.println(reverseString1(inputString));
+        System.out.println(reverseString2(inputString));
+    }
+
+    //This is ridiculously slow = 1018 ms
+    private static String reverseString(String inputString) {
+
+        String reversed = "";
+
+        int length = inputString.length();
+
+        for(int i=length-1;i>=0;i--){
+            reversed = reversed.concat(String.valueOf(inputString.charAt(i)));
+        }
+
+        return reversed;
+    }
+
+    //This is fast = 3 ms
+    private static String reverseString1(String inputString) {
+
+        StringBuilder reversed = new StringBuilder();
+
+        int length = inputString.length();
+
+        for(int i=length-1;i>=0;i--){
+            reversed = reversed.append(inputString.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+
+    // This is Fast = 3 ms
+    // https://leetcode.com/submissions/detail/183013573/
+    private static String reverseString2(String inputString) {
+
+        StringBuilder reversed = new StringBuilder();
+
+        StringBuilder input = new StringBuilder(inputString);
+
+        int length = input.length();
+
+        for(int i=length-1;i>=0;i--){
+            reversed = reversed.append(input.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+}
