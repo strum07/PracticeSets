76a04bbf7cc693e7c00d82e12b8da42153e96f1b
Sagar Sudhakar <sagarsudhakar@live.com>
2018-10-18 23:53:43 -0500

    Adding primitive types bootcamp

---
+++ src/EPI_Prep/Arrays/PrimitiveTypes/CountOfBits.java
@@ -0,0 +1,23 @@
+package EPI_Prep.Arrays.PrimitiveTypes;
+
+public class CountOfBits {
+    public static void main(String[] args) {
+        int x=3;
+
+        System.out.println(countBits(x));
+    }
+
+    private static int countBits(int number) {
+
+        short numBits =0;
+
+        while(number!=0){
+            numBits+=(number&1);
+
+            //Right Shift Operator?
+            number>>>=1;
+
+        }
+        return numBits;
+    }
+}
