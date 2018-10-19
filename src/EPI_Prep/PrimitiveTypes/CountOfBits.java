26ea6d999ea231a6c77ef1b935b62a279e842e1b
Sagar Sudhakar <sagarsudhakar@live.com>
2018-10-19 16:39:37 -0500

    Need to buck up

--- src/EPI_Prep/PrimitiveTypes/CountOfBits.java
+++ src/EPI_Prep/PrimitiveTypes/CountOfBits.java
@@ -1,23 +1,53 @@
 package EPI_Prep.PrimitiveTypes;
 
+
+// https://www.geeksforgeeks.org/what-are-the-differences-between-bitwise-and-logical-and-operators-in-cc/
+
+// In Java an int is always 32 bits
+
 public class CountOfBits {
     public static void main(String[] args) {
-        int x=3;
 
-        System.out.println(countBits(x));
+        int x=32;
+
+        System.out.println(x&1);
+
+        System.out.println("Count of Bits: "+countBits(x));
     }
 
     private static int countBits(int number) {
 
-        short numBits =0;
+        short numberOfBits =0;
+
+        int iteration =0;
 
         while(number!=0){
-            numBits+=(number&1);
 
-            //Right Shift Operator?
+            System.out.println("Iteration: "+(iteration+1));
+
+            // so that we are "AND"-ing against 0001 to the last digit to pop so see if we have a bit in there
+            // 1 will increment a count
+            // 0 will not
+            numberOfBits+=(number&1);
+
+
+
+            System.out.println("Number of Bits: "+numberOfBits);
+
+            System.out.println("Before shift: "+number);
+
+
+            // Unsigned Right Shift
+
             number>>>=1;
 
+            System.out.println("After shift: "+number);
+
+            iteration++;
+
+            System.out.println();
+
         }
-        return numBits;
+        return numberOfBits;
     }
 }
